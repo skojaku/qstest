d0d1e49bc0abf8750afeac04f34c889768fcf225
sadamori koujaku <freesailing4046@gmail.com>
2017-11-29 14:12:00 +0000

    +

--- test.py
+++ test.py
@@ -5,16 +5,16 @@ def test1():
 	network = nx.karate_club_graph()
 	communities = qs.louvain(network)
 	sg, pvals = qs.qstest(network, communities, qs.qmod, qs.vol, qs.louvain)
-	assert len(sg)==len(communities)
-	assert len(pvals)==len(communities)
-	assert max(pvals)<=1.0
-	assert min(pvals)>=0.0
+	assert len(sg) == len(communities)
+	assert len(pvals) == len(communities)
+	assert max(pvals) <= 1.0
+	assert min(pvals) >= 0.0
 
 def test2():
 	network = nx.karate_club_graph()
 	communities = qs.louvain(network)
 	sg, pvals = qs.qstest(network, communities, qs.qmod, qs.vol, qs.louvain, num_of_thread = 1)
-	assert len(sg)==len(communities)
-	assert len(pvals)==len(communities)
-	assert max(pvals)<=1.0
-	assert min(pvals)>=0.0
+	assert len(sg) == len(communities)
+	assert len(pvals) == len(communities)
+	assert max(pvals) <= 1.0
+	assert min(pvals) >= 0.0
