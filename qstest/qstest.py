79240758ed098db24d3935a5864cbfad82de6b39
Sadamori Kojaku <freesailing4046@gmail.com>
2018-03-01 22:57:32 +0000

    L

--- qstest/qstest.py
+++ qstest/qstest.py
@@ -107,8 +107,7 @@ def draw_qs_samples(network, communities, qfunc, sfunc, cdalgorithm, num_of_rand
     q_rand = []
     s_rand = []
     for i in range(num_of_rand_net):
-        #networkr = nx.configuration_model(deg)
-	networkr = nx.expected_degree_graph(deg)
+        networkr = nx.configuration_model(deg)
         communities_rand = cdalgorithm(networkr)
         q_rand = q_rand + [qfunc(networkr, x) for x in communities_rand]    
         s_rand = s_rand + [sfunc(networkr, x) for x in communities_rand]
